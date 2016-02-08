b0497065478a009f5772fffcac927372fb70905d
Shaw <shshaw@users.noreply.github.com>
2016-02-08 12:47:45 -0600

    `requestAnimationFrame` polyfill.

--- src/intro.js
+++ src/intro.js
@@ -5,7 +5,10 @@
   var canvallax = win.canvallax = win.canvallax || {},
       doc = document,
       root = doc.documentElement,
-      body = doc.body;
+      body = doc.body,
+      requestAnimationFrame = win.requestAnimationFrame || win.mozRequestAnimationFrame || win.webkitRequestAnimationFrame || win.msRequestAnimationFrame || win.oRequestAnimationFrame ||
+      // IE Fallback
+      function(callback){ win.setTimeout(callback, 20); };
 
   // Exit if browser does not support canvas
   if ( !win.CanvasRenderingContext2D ) { return false; }
