da4738047ad730091a54d1aa3ba3f9011f84c5f0
Shaw <shshaw@users.noreply.github.com>
2016-02-09 13:25:08 -0600

    canvallax.Polygon now supports custom shapes an array as the `points` value, and uses `width` & `height` instead of `radius`. Custom shapes can be closed by setting the last point as 'close'.
    Example:
    ```
    var star = canvallax.Polygon({
              fill: '#F00',
              width: 160,
              height: 150,
              points: [[80, 0], [100, 50], [160, 55], [115, 95], [130, 150], [80, 120], [30, 150], [45, 95], [0, 55], [60, 50],'close'],
            });
    ```

--- src/Elements/canvallax.Polygon.js
+++ src/Elements/canvallax.Polygon.js
@@ -8,29 +8,44 @@ canvallax.Polygon = createElement(
      */
     type: 'polygon',
 
-    sides: 6,
+    /**
+     * Number of points for standard polygons (triangle, hexagon, etc) or an array of coordinates to draw more complex shapes.
+     * @type {number|array}
+     * @default
+     */
+    points: 6,
     // (Number)
     // Number of the polygon's sides. `3` is a triangle, `4` is a square, etc.
 
-    radius: 20,
-    // (Number)
-    // Radius of the polygon.
+    width: 20,
+
+    height: 20,
 
     draw: function(ctx,coords) {
-      var i = this.sides,
-          radius = this.radius,
-          x = coords[0] + radius,
-          y = coords[1] + radius;
+      var p = this.points,
+          len = p.length,
+          i = 0, w, h, x, y;
 
+      if ( len ) {
+        for (; i < len; i++) {
+          if ( p[i] === 'close' ) { ctx.closePath(); }
+          else { ctx[( i == 0 ? 'moveTo' : 'lineTo' )](coords[0] + p[i][0], coords[1] + p[i][1]); }
+        }
+      } else {
+        w = this.width / 2,
+        h = this.height / 2,
+        x = coords[0] + w,
+        y = coords[1] + h;
 
-      ctx.moveTo(x + radius, y);
-
-      // Polygon math adapted from http://scienceprimer.com/drawing-regular-polygons-javascript-canvas
-      while (i--) {
-        ctx.lineTo(
-          x+(radius * Math.cos((i * twoPI) / this.sides)),
-          y+(radius * Math.sin((i * twoPI) / this.sides))
-        );
+        ctx.moveTo(x + w, y);
+        // Polygon math adapted from http://scienceprimer.com/drawing-regular-polygons-javascript-canvas
+        for (; i < p; i++) {
+          ctx.lineTo(
+            x+(w * Math.cos((i * twoPI) / p)),
+            y+(h * Math.sin((i * twoPI) / p))
+          );
+        }
+        ctx.closePath();
       }
     }
 
