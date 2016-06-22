345554c3f60740b2140ecba3a220f665263e9a52
Shaw <shshaw@users.noreply.github.com>
2016-06-22 11:19:15 -0500

    Update 2016-06-22T16:19:10.225Z

---
+++ plugins/canvallax.PointerExcitement.js
@@ -0,0 +1,15 @@
+(function(win){
+
+  win._clx = win._clx || [];
+  win._clx.push(function(canvallax){
+
+    'use strict';
+
+    canvallax.PointerExcitement = canvallax.createTracker({
+
+
+    });
+
+  });
+
+})(window || this);
